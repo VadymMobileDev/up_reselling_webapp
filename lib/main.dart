78637bfcc5773377aec99c60bac3c26d0396612b
Vadim <syromiatnikvs@gmail.com>
2021-08-07 08:54:02 +0300

    init 6.08

--- lib/main.dart
+++ lib/main.dart
@@ -69,6 +69,21 @@ class _XummPageState extends State<XummPage> {
                   child: Column(
                     children: [
                       SizedBox(height: Dimens.paddingMedium),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          icon: Icon(Icons.person),
+                          hintText: 'What do people call you?',
+                          labelText: 'Name *',
+                        ),
+                        autofocus: true,
+                        onSaved: (String? value) {
+                          // This optional block of code can be used to run
+                          // code when the user saves the form.
+                        },
+                        validator: (String? value) {
+                          return (value != null && value.contains('@')) ? 'Do not use the @ char.' : null;
+                        },
+                      ),
                       BeckToHomeOpenWidget(),
                       RegisterManageDomainPage(),
                       CardUnstoppableWidget(),
