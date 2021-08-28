b6e08f565eb55a8443450450624acd88459be2b5
Vadim <syromiatnikvs@gmail.com>
2021-08-28 19:36:36 +0300

    init 30.08

--- lib/widgets/more_about_program/more_about_program_page.dart
+++ lib/widgets/more_about_program/more_about_program_page.dart
@@ -3,7 +3,7 @@ import 'package:up_reselling_webapp/application/app_color.dart';
 import 'package:up_reselling_webapp/application/style/dimens.dart';
 
 const _spacePadding = const EdgeInsets.only(
-  top: Dimens.paddingMediumLarge,
+  top: Dimens.paddingLarge,
   left: Dimens.paddingMedium,
   right: Dimens.paddingMedium,
   bottom: Dimens.paddingMediumLarge,
@@ -17,77 +17,89 @@ class MoreAboutProgramingPage extends StatefulWidget {
 }
 
 class _MoreAboutProgramingState extends State<MoreAboutProgramingPage> {
-
   TextEditingController domainController = TextEditingController();
 
   @override
-  Widget build(BuildContext context) =>
-      Scaffold(
+  Widget build(BuildContext context) => Scaffold(
+    backgroundColor: AppColor.backgroundLightBlue,
         body: Padding(
-              padding: _spacePadding,
-              child: Column(
-                children: [
-                  Card(
-                      shape: RoundedRectangleBorder(
-                        side: BorderSide(color: AppColor.textGreyMiddle, width: 0.5),
-                        borderRadius: BorderRadius.circular(6),
+            padding: _spacePadding,
+            child: Column(
+              children: [
+                Card(
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(color: AppColor.textGreyMiddle, width: 0.5),
+                      borderRadius: BorderRadius.circular(6),
+                    ),
+                    child: Center(
+                        child: MergeSemantics(
+                            child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        children: <Widget>[
+                          Text("Give \$10, Get \$10",
+                              style: TextStyle(
+                                  fontSize: Dimens.borderButtonRadius,
+                                  color: AppColor.textBlue,
+                                  fontWeight: FontWeight.bold)),
+                          SizedBox(height: Dimens.paddingMediumLarge),
+                          Text(
+                              "Refer a friend with a \$10 credit and \n earn \$10 credit after they make their \nfirst purchase.",
+                              style: TextStyle(color: AppColor.textGrey),
+                            textAlign: TextAlign.center),
+                          SizedBox(height: Dimens.paddingMediumLarge),
+                          Card(
+                            color: AppColor.backgroundLightGrey,
+                            elevation: 0.0,
+                            shape: RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(Dimens.paddingDefault)),
+                                side: BorderSide(width: 2, color: AppColor.borderCardGrey)),
+                            child: Padding(
+                              padding: EdgeInsets.all(Dimens.paddingSemi),
+                              child: Row(
+                                children: <Widget>[
+                                  Expanded(
+                                      child: Align(
+                                          alignment: Alignment.centerLeft,
+                                          child: TextField(
+                                            enabled: false,
+                                            maxLines: 2,
+                                            controller: domainController,
+                                            decoration: InputDecoration(
+                                              counterText: '',
+                                              enabledBorder: InputBorder.none,
+                                              errorBorder: InputBorder.none,
+                                              disabledBorder: InputBorder.none,
+                                              isDense: true,
+                                              hintText: "https://unstoppabledomains.com/r/2 2b214f68524f2",
+                                              hintStyle: TextStyle(color: AppColor.black),
+                                            ),
+                                          ))),
+                                ],
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: Dimens.paddingMedium),
+                          ElevatedButton(
+                              onPressed: () {},
+                              child: Text("Copy Link"),
+                              style: ElevatedButton.styleFrom(
+                                  elevation: 0.0,
+                                  primary: AppColor.primaryBlue,
+                                  minimumSize: Size(double.infinity, Dimens.margeButtonEdge),
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(Dimens.paddingDefault)))),
+                        ],
                       ),
-                      child: Center(
-                          child: MergeSemantics(
-                              child: Padding(
-                                padding: const EdgeInsets.all(16.0),
-                                child: Column(
-                                  children: <Widget>[
-                                    Text("View Receipt",
-                                        style: TextStyle(
-                                            color: AppColor.textBlue, fontWeight: FontWeight.bold)),
-                                    Text("We've emailed your receipt to",
-                                        style: TextStyle(color: AppColor.textGrey)),
-                                    Card(
-                                      color: AppColor.backgroundLightGrey,
-                                      elevation: 0.0,
-                                      shape: RoundedRectangleBorder(
-                                          borderRadius: BorderRadius.all(Radius.circular(Dimens.paddingDefault)),
-                                          side: BorderSide(width: 2, color: AppColor.borderCardGrey)),
-                                      child: Padding(
-                                        padding: EdgeInsets.all(Dimens.paddingSemi),
-                                        child: Row(
-                                          children: <Widget>[
-                                            Expanded(
-                                                child: Align(
-                                                    alignment: Alignment.centerLeft,
-                                                    child: TextField(
-                                                      enabled: true,
-                                                      controller: domainController,
-                                                      decoration: InputDecoration(
-                                                        counterText: '',
-                                                        focusedBorder: InputBorder.none,
-                                                        enabledBorder: InputBorder.none,
-                                                        errorBorder: InputBorder.none,
-                                                        disabledBorder: InputBorder.none,
-                                                        isDense: true,
-                                                        hintText: "Domain name",
-                                                      ),
-                                                    ))),
-                                          ],
-                                        ),
-                                      ),
-                                    ),
-
-                                    SizedBox(height: Dimens.paddingMedium),
-                                    ElevatedButton(
-                                        onPressed: () {},
-                                        child: Text("Copy Link"),
-                                        style: ElevatedButton.styleFrom(
-                                            elevation: 0.0,
-                                            primary: AppColor.primaryBlue,
-                                            minimumSize: Size(double.infinity, Dimens.margeButtonEdge),
-                                            shape: RoundedRectangleBorder(
-                                                borderRadius: BorderRadius.circular(Dimens.paddingDefault)))),
-                                  ],
-                                ),
-                              )))),
-                  SizedBox(height: Dimens.paddingMedium),
-                ],)),
-  );
+                    )))),
+                SizedBox(height: Dimens.paddingLarge),
+                Text("Learn more about the program",
+                    style: TextStyle(
+                        fontSize: Dimens.paddingMedium,
+                        color: AppColor.textBlue,
+                        fontWeight: FontWeight.bold)),
+              ],
+            )),
+      );
 }
